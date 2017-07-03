1cabd617506fefff2292fc1698435e950ed96ada
kvmanohar22 <kvmanohar22@gmail.com>
2017-07-03 06:21:29 +0530

    Initialise test cases

---
+++ src/caffe/test/test_squeezedet_loss_layer.cpp
@@ -0,0 +1,52 @@
+#include <cmath>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "caffe/blob.hpp"
+#include "caffe/common.hpp"
+#include "caffe/filler.hpp"
+#include "caffe/layers/squeezedet_loss_layer.hpp"
+
+#include "caffe/test/test_caffe_main.hpp"
+#include "caffe/test/test_gradient_check_util.hpp"
+
+namespace caffe {
+
+template <typename TypeParam>
+class SqueezeDetLossLayerTest : public MultiDeviceTest<TypeParam> {
+  typedef typename TypeParam::Dtype Dtype;
+
+ protected:
+  SqueezeDetLossLayerTest()
+      : blob_bottom_data_(new Blob<Dtype>(1, 7, 7, 20)),
+        blob_bottom_label_(new Blob<Dtype>(6, 1, 1, 1)),
+        blob_top_loss_(new Blob<Dtype>()) {
+
+    // Fill the data
+    FillerParameter filler_param;
+    filler_param.set_std(10);
+    GaussianFiller<Dtype> filler(filler_param);
+    filler.Fill(this->blob_bottom_data_);
+    blob_bottom_vec_.push_back(blob_bottom_data_);
+    for (int i = 0; i < blob_bottom_label_.count(); ++i) {
+      blob_bottom_label_->mutable_cpu_data()[i] = caffe_rng_rand() % 5;
+    }
+    blob_bottom_vec_.push_back(blob_bottom_label_);
+    blob_top_vec_.push_back(blob_top_loss_);
+  }
+  virtual ~SqueezeDetLossLayerTest() {
+    delete blob_bottom_data_;
+    delete blob_bottom_label_;
+    delete blob_top_loss_;
+  }
+  Blob<Dtype>* const blob_bottom_data_;
+  Blob<Dtype>* const blob_bottom_label_;
+  Blob<Dtype>* const blob_top_loss_;
+  vector<Blob<Dtype>*> blob_bottom_vec_;
+  vector<Blob<Dtype>*> blob_top_vec_;
+};
+
+TYPED_TEST_CASE(SqueezeDetLossLayerTest, TestDtypesAndDevices);
+
+}  // namespace caffe
